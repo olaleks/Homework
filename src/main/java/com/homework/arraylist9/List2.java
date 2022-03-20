fa18dadcdb5a1105c05bf5aafeb432bca59524ed
kirill.simonovich <kiril.simonovich@gmail.com>
2022-03-20 12:55:45 +0300

    fix list2

--- src/main/java/com/homework/arraylist9/List2.java
+++ src/main/java/com/homework/arraylist9/List2.java
@@ -2,15 +2,25 @@ package com.homework.arraylist9;
 
 import java.util.ArrayList;
 
+/**
+ * В классе List2 есть метод printStudentNames(ArrayList), который принимает список объектов и выводит их имена.
+ * Но на данный момент программа не компилируется.
+ * Твоя задача исправить этот баг. Для этого нужно исправить сигнатуру метода printStudentNames(ArrayList), чтобы он принимал список объектов только типа Student(используй дженерик).
+ * Метод main не участвует в проверке.
+ * Требования:
+ * •	Исправь сигнатуру метода printStudentNames, чтобы он принимал список только студентов.
+ * •	Реализацию метода printStudentNames не изменять.
+ */
+
 public class List2 {
 
-    public static void main(String[] args) {
-        ArrayList<Student> students = new ArrayList<>();
-        students.add(new Student("Акакий"));
-        students.add(new Student("Любослав"));
+  public static void main(String[] args) {
+    ArrayList<Student> students = new ArrayList<>();
+    students.add(new Student("Акакий"));
+    students.add(new Student("Любослав"));
 
-        printStudentNames(students);
-    }
+    printStudentNames(students);
+  }
 
     public static void printStudentNames(ArrayList<Student> students) {
         for (int i = 0; i < students.size(); i++) {
