66c58bfbda55a874a92f4e862fc07dc8524b3ef1
olgasalachyonok <olgasalachyonok@gmail.com>
2022-04-16 10:39:08 -0500

    class  StringComparator added

---
+++ src/main/java/com/homework/lambda_stream12/StringComparator.java
@@ -0,0 +1,10 @@
+package com.homework.lambda_stream12;
+
+import java.util.Comparator;
+
+public class StringComparator implements Comparator<String> {
+    @Override
+    public int compare(String s1, String s2) {
+        return s1.length() - s2.length();
+    }
+}
